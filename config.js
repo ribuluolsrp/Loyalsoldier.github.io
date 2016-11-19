06ae5b0556989941ab48f58d8b2ce8b550b443c1
loyalsoldier <loyalsoldier@outlook.com>
2016-11-19 14:50:00 +0800

    修改 Ghost 配置文件中的域名

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://www.loyalsoldier.me',
+        url: 'https://www.loyalsoldier.me',
         mail: {},
         database: {
             client: 'sqlite3',
